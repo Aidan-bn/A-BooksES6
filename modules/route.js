773a2320efdedf7636f21a62a17380096ed2725d
Aidan - bn <81457065+Aidan-bn@users.noreply.github.com>
2022-09-26 17:39:46 +0300

    Resolve linter errors

--- modules/route.js
+++ modules/route.js
@@ -1,26 +1,25 @@
 import {
-    formSec, contSec, listSec, listMenu, formMenu, contactMenu,
-  } from './declaration.js';
-  
-  const navigationBar = () => {
-    listMenu.addEventListener('click', () => {
-      formSec.style.display = 'none';
-      contSec.style.display = 'none';
-      listSec.style.display = 'block';
-    });
-  
-    formMenu.addEventListener('click', () => {
-      formSec.style.display = 'block';
-      contSec.style.display = 'none';
-      listSec.style.display = 'none';
-    });
-  
-    contactMenu.addEventListener('click', () => {
-      formSec.style.display = 'none';
-      contSec.style.display = 'block';
-      listSec.style.display = 'none';
-    });
-  };
-  
-  export default navigationBar;
-  
+  formSec, contSec, listSec, listMenu, formMenu, contactMenu,
+} from './declaration.js';
+
+const navigationBar = () => {
+  listMenu.addEventListener('click', () => {
+    formSec.style.display = 'none';
+    contSec.style.display = 'none';
+    listSec.style.display = 'block';
+  });
+
+  formMenu.addEventListener('click', () => {
+    formSec.style.display = 'block';
+    contSec.style.display = 'none';
+    listSec.style.display = 'none';
+  });
+
+  contactMenu.addEventListener('click', () => {
+    formSec.style.display = 'none';
+    contSec.style.display = 'block';
+    listSec.style.display = 'none';
+  });
+};
+
+export default navigationBar;
